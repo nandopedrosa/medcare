fa783f7c532a25d9cfcb02dc57579fc4e7998199
nandopedrosa <fpedrosa@gmail.com>
2020-05-02 12:06:57 -0300

    change procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp target/classes:target/dependency/* app.Application
+web: java $JAVA_OPTS -jar /app/tmp/cache/.m2/repository/app/medcare/1.0-SNAPSHOT/medcare-1.0-SNAPSHOT-jar-with-dependencies.jar
--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -26,8 +26,7 @@ public class Application {
         // This tells our app that if Heroku sets a port for us, we need to use that
         // port.
         // Otherwise, if they do not, continue using port 4567.
-        if (process.environment().get("PORT") != null) {
-            System.out.println("ENTROU NO PORT");
+        if (process.environment().get("PORT") != null) {            
             port = Integer.parseInt(process.environment().get("PORT"));
         } else {
             port = 4567;
